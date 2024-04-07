dc47d51e047d94c69e187443cb9548cbe87f9260
Gger206 <arteml22012006@gmail.com>
2024-04-07 20:54:05 +0300

    first task

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "libs/data_structures/matrix/matrix.h"
+
+void swapMinAndMaxRows(matrix m) {
+    position minPos = getMinValuePos(m);
+    position maxPos = getMaxValuePos(m);
+
+    swapRows(&m, minPos.rowIndex, maxPos.rowIndex);
+}
+
+int getMax(int *a, int n) {
+    int max = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    return max;
+}
+
+int getMin(int *a, int n) {
+    int min = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] < min) {
+            min = a[i];
+        }
+    }
+    return min;
+}
+
+matrix mulMatrices(matrix m1, matrix m2) {
+    matrix result = getMemMatrix(m1.nRows, m2.nCols);
+
+    for (int i = 0; i < m1.nRows; i++) {
+        for (int j = 0; j < m2.nCols; j++) {
+            for (int k = 0; k < m1.nCols; k++) {
+                result.values[i][j] += m1.values[i][k] * m2.values[k][j];
+            }
+        }
+    }
+
+    return result;
+}
